cc4c733895cdd12eb738dfe3258218da1ad9e05f
Mohit <hydarkdevil@gmail.com>
2026-06-18 16:11:23 +0530

    fix auth split role route import

--- backend/controllers/authLogin.controller.js
+++ backend/controllers/authLogin.controller.js
@@ -64,6 +64,7 @@ import {
   registrationProfile,
   resolveCanonicalIdentity,
   roleGuidance,
+  ROLE_ROUTES,
   scheduleLoginMaintenance,
   sessionUserFromAuthenticatedRequest,
   setAuthCookie,
